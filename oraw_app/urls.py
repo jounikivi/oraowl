e1a9995efcaac28a9c0410707e003401bd366114
Jouni Kiviperä <jounikivipera@gmail.com>
2025-11-04 07:58:12 +0200

    Update urls.py

--- oraw_app/urls.py
+++ oraw_app/urls.py
@@ -28,20 +28,25 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("upload/iofxml/", views.UploadIOFXMLView.as_view(), name="upload_iofxml"),
     path("competitions/", views.CompetitionListView.as_view(), name="competitions_index"),
-    path("competitions/<uuid:pk>/", views.CompetitionDetailView.as_view(),
-         name="competitions_detail",
+    path("competitions/<uuid:pk>/",
+        views.CompetitionDetailView.as_view(),
+        name="competitions_detail",
     ),
     path("athletes/", views.AthleteListView.as_view(), name="athletes_index"),
     path("athletes/<uuid:pk>/", views.AthleteDetailView.as_view(), name="athletes_detail"),
     
        # Login & Logout
     path(
-        "accounts/login/",LoginView.as_view(template_name="oraw_app/accounts/login.html"),
+        "accounts/login/",
+        LoginView.as_view(template_name="oraw_app/accounts/login.html"),
         name="login",
     ),
-    path("accounts/logout/",LogoutView.as_view(next_page="oraw_app:home"),
+    path(
+        "accounts/logout/",
+        LogoutView.as_view(next_page="oraw_app:home"),
         name="logout",
     ),
+
     # Signup (rekisteröinti) – toteutamme seuraavassa kohdassa SignUpView:n
     path("accounts/signup/", views.SignUpView.as_view(), name="signup"),
 ]
