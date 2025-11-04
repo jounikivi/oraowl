9971490143c6aa93f0fa4b5a6cd0c939cbe787da
Jouni Kiviperä <jounikivipera@gmail.com>
2025-11-04 07:57:15 +0200

    Update urls.py

--- oraw_app/urls.py
+++ oraw_app/urls.py
@@ -16,20 +16,33 @@ Including another URLconf
 """
 
 from django.urls import path
+from django.contrib.auth import views as auth_views
+from django.contrib.auth.views import LogoutView, LoginView
 from . import views
 
+# FI: Namespace templatelle. 
+# EN: URL namespace.
 app_name = "oraw_app"
 
 urlpatterns = [
     path("", views.home, name="home"),
     path("upload/iofxml/", views.UploadIOFXMLView.as_view(), name="upload_iofxml"),
     path("competitions/", views.CompetitionListView.as_view(), name="competitions_index"),
-    path(
-        "competitions/<uuid:pk>/",
-        views.CompetitionDetailView.as_view(),
-        name="competitions_detail",
+    path("competitions/<uuid:pk>/", views.CompetitionDetailView.as_view(),
+         name="competitions_detail",
     ),
     path("athletes/", views.AthleteListView.as_view(), name="athletes_index"),
     path("athletes/<uuid:pk>/", views.AthleteDetailView.as_view(), name="athletes_detail"),
+    
+       # Login & Logout
+    path(
+        "accounts/login/",LoginView.as_view(template_name="oraw_app/accounts/login.html"),
+        name="login",
+    ),
+    path("accounts/logout/",LogoutView.as_view(next_page="oraw_app:home"),
+        name="logout",
+    ),
+    # Signup (rekisteröinti) – toteutamme seuraavassa kohdassa SignUpView:n
+    path("accounts/signup/", views.SignUpView.as_view(), name="signup"),
 ]
 
