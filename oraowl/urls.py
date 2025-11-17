750495738b868fc6b29a766a4e84b92304ed77a1
Jouni Kiviperä <jounikivipera@gmail.com>
2025-11-17 08:50:52 +0200

    korjauksia

--- oraowl/urls.py
+++ oraowl/urls.py
@@ -20,5 +20,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path("admin/", admin.site.urls),
-    path("", include("oraw_app.urls")),
+    path("", include("oraw_app.urls", namespace="oraw_app")),
 ]
